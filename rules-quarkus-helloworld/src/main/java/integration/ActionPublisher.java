7ca4fe372ac72dd2b91062a201ca38a2424b86a7
Donato Marrazzo <donato.marrazzo@gmail.com>
2021-03-12 18:24:57 +0100

    query publisher

--- rules-quarkus-helloworld/src/main/java/integration/ActionPublisher.java
+++ rules-quarkus-helloworld/src/main/java/integration/ActionPublisher.java
@@ -1,17 +1,32 @@
 package integration;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import org.jboss.logging.Logger;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+import org.kie.api.runtime.rule.QueryResults;
 
 import io.quarkus.vertx.ConsumeEvent;
+import service.SessionHolder;
 
 @ApplicationScoped
 public class ActionPublisher {
     private static final Logger LOG = Logger.getLogger(ActionPublisher.class);
 
+    @Inject
+    SessionHolder sessionHolder;
+
     @ConsumeEvent(value = "rules-fired", blocking = true)
     public void action(Object object) {
         LOG.info("rules fired");
+        KieSession kieSession = sessionHolder.getKieSession();
+        QueryResults results = kieSession.getQueryResults("all worlds");
+        results.forEach(r -> {
+            FactHandle factHandle = r.getFactHandle("$s");
+            LOG.info(kieSession.getObject(factHandle));
+            kieSession.delete(factHandle);
+        });
     }
 }
